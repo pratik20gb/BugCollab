f43f63c9e16236a301e7a24a51be57301056f8db
shori <pratik20gb@gmail.com>
2025-10-06 12:14:40 +0530

    exploring regex

--- src/app.js
+++ src/app.js
@@ -1,17 +1,13 @@
 const express = require("express");
 const app = express();
+// app.get("/ab\\+c", (req, res) => {
+//   res.send("the or chaining");
+// });
 
-app.get("/", (req, res) => {
-  res.send({
-    message: "This is the get/ route ",
-  });
+app.get(/^\/ab+c$/, (req, res) => {
+  res.send("regex route working");
 });
-app.post("/user", (req, res) => {
-  res.send("data saved inside database");
-});
-app.delete("/user", (req, res) => {
-  res.send("data deleted successfully");
-});
+
 
 app.listen(3000, () => {
   console.log("server is listening on port 3000");
