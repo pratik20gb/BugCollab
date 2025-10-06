132a0e3685bec82530c5ee02837489b5b90da88c
shori <pratik20gb@gmail.com>
2025-10-06 12:20:50 +0530

    req -query -params

--- src/app.js
+++ src/app.js
@@ -1,14 +1,11 @@
 const express = require("express");
 const app = express();
-// app.get("/ab\\+c", (req, res) => {
-//   res.send("the or chaining");
-// });
-
-app.get(/^\/ab+c$/, (req, res) => {
-  res.send("regex route working");
+app.get("/test", (req, res) => {
+  console.log(req.query);
 });
-
-
+app.get("/user/:userId/:name/:password", (req, res) => {
+  console.log(req.params);
+});
 app.listen(3000, () => {
   console.log("server is listening on port 3000");
 });
